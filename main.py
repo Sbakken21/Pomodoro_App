7929e74e29c004e02594120951561915972caf7e
Sbakken21 <sbakken21@gmail.com>
2017-07-18 08:25:08 -0500

    Added sound effect and corrected 'skip' button

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.clock import Clock
 from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
+from kivy.core.audio import SoundLoader
 
-class Timer(Widget):
+class Timer():
     
     work_time, default = 10, 10
     break_time = 5
     long_break = 15
-    sequence = ['w', 'b', 'w', 'b', 'w', 'b', 'w', 'lb']
+    sequence = ['Work', 'Break', 'Work', 'Break', 'Work', 'Break', 'Work', 'Long Break']
     cycle = 0
-    pomo_count = 0
+    pomo_count = 1
     timer_On = False
 
     # Function to convert time into minutes and seconds
@@ -28,19 +30,19 @@ class Timer(Widget):
     def stop_timer(self):
         self.timer_On = False
         
-    # this should go back to the 'default' display
+    # this should go back to the 'default' display and reset all counts
     def reset_timer(self):
+        self.cycle = 0
+        self.pomo_count = 1
         self.timer_On = False
         self.work_time = self.default 
 
     def break_timer(self):
-        self.pomo_count = 0
-        self.cycle = 0
-        self.timer_On = True
-        if self.sequence != 'lb' or 'w':
-            self.work_time = self.break_time
-        else:
-            self.work_time = self.long_break     # THIS NEEDS TO BE ADJUSTED, FOR W, B, AND LB (CURRENTLY SET UP ONLY FOR TESTING)
+        self.cycle += 1
+        # if self.sequence != 'Long Break' or 'Work':
+        #     self.work_time = self.break_time
+        # else:
+        #     self.work_time = self.long_break     # THIS NEEDS TO BE ADJUSTED, FOR W, B, AND LB (CURRENTLY SET UP ONLY FOR TESTING)
     
 class PomoLayout(Widget):
     
@@ -48,33 +50,40 @@ class PomoLayout(Widget):
     displayLabel = ObjectProperty(None)
     cycleLabel = ObjectProperty(None)
     
+    timer_alarm = SoundLoader.load('Resources/screamingsun.mp3')
+
+    # Checks at end of timer to see how many cycles are completed 
     def check_cycle(self):
-        self.cycleLabel.text = 'Completed Cycles: ' + str(self.timer.pomo_count)
-        if self.timer.cycle < 3:
+        # self.timer_alarm = SoundLoader.load('Resources/screamingsun.mp3')
+        if self.timer.cycle < 7:
             self.timer.cycle += 1
         else:
             self.timer.cycle = 0
         self.setup()
 
-
+    # Based on number of cycles determines the "Status" of the timer
     def setup(self):
-        if self.timer.sequence[self.timer.cycle] == 'w':
+        self.timer_alarm.play()
+        self.statusLabel.text = str('Status: ' + self.timer.sequence[self.timer.cycle])
+        if self.timer.sequence[self.timer.cycle] == 'Work':
             self.timer.work_time = self.timer.default
             self.timer.pomo_count += 1
-            if self.timer.pomo_count == 5:
+            if self.timer.pomo_count == 4:
                 self.timer.pomo_count = 0
                 
-        elif self.timer.sequence[self.timer.cycle] == 'b':
+        elif self.timer.sequence[self.timer.cycle] == 'Break':
+            self.cycleLabel.text = 'Completed Cycles: ' + str(self.timer.pomo_count) + '/4'
             self.timer.work_time = self.timer.break_time
         else:
             self.timer.work_time = self.timer.long_break
         self.btn_default()
         # self.cycle_display()
         
-
+    # The default time on the timer
     def default_display(self):
         return str(self.timer.time_convert())
 
+    # The default cycle_display MAYBE DELETE THIS IF IT ISNT BEING USED
     def cycle_display(self):
         print('Count: ' + str(self.timer.pomo_count))
         return 'Completed cycles: ' + str(self.timer.pomo_count)
@@ -94,12 +103,16 @@ class PomoLayout(Widget):
             self.timer.start_timer()
     
     def break_button(self):
-        self.timer.break_timer()
+        self.timer.cycle += 1
+        self.timer.timer_On = True
+        self.setup()
 
     def reset_button(self):
         self.reset.disabled = True
         self.timer.reset_timer()
         self.displayLabel.text = self.default_display()
+        self.cycleLabel.text = 'Completed Cycles: 0/4'
+        self.statusLabel.text = 'Status: Work'
         self.btn_default()
  
 
@@ -107,13 +120,14 @@ class PomoLayout(Widget):
         if self.timer.timer_On and self.timer.work_time > 0:
             self.reset.disabled = False
             self.timer.work_time -= 1
-            print(self.timer.work_time)
+            print(self.timer.work_time) # REMOVE THIS CODE LATER
             self.displayLabel.text = self.timer.time_convert()
         elif self.timer.work_time == 0:
             self.timer.timer_On = False
             self.displayLabel.text = 'Time is Up!!!' # change this for different cycle stuff
             self.check_cycle()
-            
+###########################################################################################################
+
 
 
 class PomoApp(App):
