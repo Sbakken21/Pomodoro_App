9acc69f62a4e0373c3d154b165f58af27e4eaa2b
Sbakken21 <sbakken21@gmail.com>
2018-01-11 01:04:17 -0600

    updated window size and text display

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Pomodoro_App
 Pomodoro App using Kivy.
-This is an app made using the Kivy framework in Python. The app uses the pomodoro technique for the timer for time management.
+This is an app made using the Kivy framework in Python. The timer uses the popular time management system, the pomodoro technique, to cycle through "work" and "break" periods.
+
+## How it works
+For the "work" periods,  the timer will countdown from 25 minutes. After the 25 minute "work" cycle is completed, a 5 minute break is allowed. After completing 4 consecutive "work" cycles, a 15 minute "long break" timer will begin.
+
+## Features
+* Pomodoro timer that automatically cycles through work, break, and long break timers following standard pomodoro technique guidelines.
+* A task/todo list 
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
-from kivy.properties import ObjectProperty, ListProperty  # REMOVE LIST PROPERTY IF NOT USED
+from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
 from kivy.core.audio import SoundLoader
-from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.listview import ListItemButton
-from kivy.uix.popup import Popup
-from kivy.lang import Builder
+from kivy.config import Config
 
+# Settings for window display
+Config.set('graphics', 'width', '400')
+Config.set('graphics', 'height', '600')
+Config.set('graphics', 'resizable', '0')
+Config.write()
 
 class Timer(Widget):
     work_time, default = 1500, 1500
@@ -114,11 +118,8 @@ class Timer(Widget):
         elif self.work_time == 0:
             self.timer_On = False
             self.displayLabel.text = "Time is Up!!!"
+            self.displayLabel.font_size = "50"
             self.check_cycle()
-
-
-
-
 
 class TaskButton(ListItemButton):
     pass
